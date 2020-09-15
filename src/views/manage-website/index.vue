2b9ed337833352510244871a787b716d636a87bc
caiwenglong <836472117@qq.com>
2020-09-15 15:15:29 +0800

    删除无用的框架自带的文件

--- src/views/manage-website/index.vue
+++ src/views/manage-website/index.vue
@@ -4,113 +4,129 @@
       <el-button
         size="mini"
         type="danger"
-        @click="handleBatchDelete()">批量删除</el-button>
+        @click="handleBatchDelete()"
+      >批量删除</el-button>
     </div>
     <el-table
       ref="multipleTableWebsite"
+      v-loading="listLoading"
       :data="tableData"
       tooltip-effect="dark"
       style="width: 100%"
       :default-sort="{prop: 'dateModified', order: 'descending'}"
-      @selection-change="handleSelectionChange">
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column
         header-align="center"
         align="center"
         type="selection"
-        width="55">
-      </el-table-column>
+        width="55"
+      />
       <el-table-column
         header-align="center"
         align="center"
         prop="name"
         label="网站名称"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        header-align="center"
-        align="center"
-        prop="address"
-        label="网站地址"
-        show-overflow-tooltip>
-      </el-table-column>
+        width="120"
+      />
       <el-table-column
         header-align="center"
         align="center"
         prop="url"
+        label="网站地址"
+        show-overflow-tooltip
+      />
+      <el-table-column
+        header-align="center"
+        align="center"
+        prop="logo"
         label="网站logo"
-        show-overflow-tooltip>
-        <template slot-scope="scope"><a class="logo-wrapper" :href="scope.row.address"><img :src="scope.row.url" alt="logo"></a></template>
+        show-overflow-tooltip
+      >
+        <template slot-scope="scope">
+          <a class="logo-wrapper" :href="scope.row.url">
+            <img :src="scope.row.logo" alt="logo">
+          </a>
+        </template>
       </el-table-column>
       <el-table-column
         header-align="center"
         align="center"
         prop="hitsToday"
         label="今日点击量"
-        width="100">
-      </el-table-column>
+        width="100"
+      />
       <el-table-column
         header-align="center"
         align="center"
         prop="hitsTotal"
         label="总点击量"
-        width="100">
-      </el-table-column>
+        width="100"
+      />
       <el-table-column
         header-align="center"
         align="center"
         label="创建日期"
         width="120"
-        prop="dateCreate"
-        sortable>
-        <template slot-scope="scope">{{ scope.row.dateCreate }}</template>
+        prop="gmtCreate"
+        sortable
+      >
+        <template slot-scope="scope">{{ scope.row.gmtCreate }}</template>
       </el-table-column>
       <el-table-column
         header-align="center"
         align="center"
         label="修改日期"
         width="120"
-        prop="dateModified"
-        sortable>
-        <template slot-scope="scope">{{ scope.row.dateModified }}</template>
+        prop="gmtModified"
+        sortable
+      >
+        <template slot-scope="scope">{{ scope.row.gmtModified }}</template>
       </el-table-column>
       <el-table-column
         header-align="center"
         align="center"
-        label="操作">
+        label="操作"
+      >
         <template slot="header">
           <el-input
             v-model="keyValue"
             size="mini"
-            placeholder="输入关键字搜索">
-            <i slot="suffix" class="el-input__icon el-icon-search"></i>
+            placeholder="输入关键字搜索"
+          >
+            <i slot="suffix" class="el-input__icon el-icon-search" />
           </el-input>
         </template>
         <template slot-scope="scope">
           <el-button
             size="mini"
-            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            @click="handleEdit(scope.$index, scope.row)"
+          >编辑</el-button>
           <el-button
             size="mini"
             type="danger"
-            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            @click="handleDelete(scope.$index, scope.row)"
+          >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
     <div class="pagination-wrapper">
       <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
+        :page-size="5"
         :current-page="currentPage"
         :page-sizes="[5, 10, 20, 50, 100]"
-        :page-size="5"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="tableData.length">
-      </el-pagination>
+        :total="tableData.length"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   const tableData = [{
     dateCreate: '2016-05-03',
     dateModified: '2020-05-08',
@@ -119,49 +135,34 @@
     hitsTotal: '123123',
     address: 'https://www.baidu.com/',
     url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  }, {
-    dateCreate: '2016-05-02',
-    dateModified: '2020-05-07',
-    name: '百度',
-    hitsToday: '123',
-    hitsTotal: '123123',
-    address: 'https://www.baidu.com/',
-    url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  }, {
-    dateCreate: '2016-05-04',
-    dateModified: '2020-05-06',
-    name: '百度',
-    hitsToday: '123',
-    hitsTotal: '123123',
-    address: 'https://www.baidu.com/',
-    url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  }, {
-    dateCreate: '2016-05-01',
-    dateModified: '2020-05-05',
-    name: '百度',
-    hitsToday: '123',
-    hitsTotal: '123123',
-    address: 'https://www.baidu.com/',
-    url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  }, {
-    dateCreate: '2016-05-07',
-    dateModified: '2020-05-01',
-    name: '百度',
-    hitsToday: '123',
-    hitsTotal: '123123',
-    address: 'https://www.baidu.com/',
-    url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
   }]
+  import { getList } from '@/api/table'
   export default {
     data() {
       return {
+        listLoading: false,
         tableData: tableData, // 表格数据
         multipleSelection: [], // 选择的数据项
         keyValue: '', // 关键字搜索
         currentPage: 1 // 当前页
       }
     },
+    created() {
+      this.fetchData()
+    },
+    computed: {
+      ...mapGetters([
+        'idAdmin'
+      ])
+    },
     methods: {
+      fetchData() {
+        this.listLoading = true
+        getList(this.idAdmin).then(response => {
+          this.tableData = response.data.tbWebsites
+          this.listLoading = false
+        })
+      },
       handleSelectionChange(val) {
         console.log(val)
         this.multipleSelection = val
